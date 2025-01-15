d4d071911cd8f8625ad9dfd2ea52bb69f09d4923
Leroy <lsharp256@gmail.com>
2025-01-16 01:39:13 +0200

    added more page objects, helperBase and pageManager

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,36 +1,50 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
+import { HelperBase } from "./helperBase";
 
-export class NavigationPage {
+export class NavigationPage extends HelperBase{
 
-    readonly page: Page
+    // This approach could potentially be difficult to maintain as the list of locators grow.
+    // Another approach is to keep the locators in the method for easier debugging and maintanence.
+
+    readonly formLayoutsMenuItem: Locator
+    readonly datePickerMenuItem: Locator
+    readonly smartTableMenuItem: Locator
+    readonly toastrMenuItem: Locator
+    readonly tooltipMenuItem: Locator
 
     constructor(page: Page){
-        this.page = page
+        super(page)
+        this.formLayoutsMenuItem = page.getByText('Form Layouts')
+        this.datePickerMenuItem = page.getByText('Datepicker')
+        this.smartTableMenuItem = page.getByText('Smart Table')
+        this.toastrMenuItem = page.getByText('Toastr')
+        this.tooltipMenuItem = page.getByText('Tooltip')
     }
     
     async formLayoutsPage(){
         await this.selectGroupMenuItem('Forms')
-        await this.page.getByText('Form Layouts').click()
+        await this.formLayoutsMenuItem.click()
+        await this.waitForNumberOfSeconds(2)
     }
 
     async datepickerPage(){
         await this.selectGroupMenuItem('Forms')
-        await this.page.getByText('Datepicker').click()
+        await this.datePickerMenuItem.click()
     }
 
     async smartTablePage(){
         await this.selectGroupMenuItem('Tables & Data')
-        await this.page.getByText('Smart Table').click()
+        await this.smartTableMenuItem.click()
     }
 
     async toastrPage(){
         await this.selectGroupMenuItem('Modal & Overlays')
-        await this.page.getByText('Toastr').click()
+        await this.toastrMenuItem.click()
     }
 
     async tooltipPage(){
         await this.selectGroupMenuItem('Modal & Overlays')
-        await this.page.getByText('Tooltip').click()
+        await this.tooltipMenuItem.click()
     }
 
     private async selectGroupMenuItem(groupItemTitle: string){
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,15 +1,26 @@
 import {expect, test} from '@playwright/test'
-import {NavigationPage} from '../page-objects/navigationPage'
+import { PageManager } from '../page-objects/pageManager'
+
 
 test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200/')
 })
 
 test('navigate to form page', async({page}) => {
-    const navigateTo = new NavigationPage(page)
-    await navigateTo.formLayoutsPage()
-    await navigateTo.datepickerPage()
-    await navigateTo.smartTablePage()
-    await navigateTo.toastrPage()
-    await navigateTo.tooltipPage()
+    const pm = new PageManager(page)
+    await pm.navigateTo().formLayoutsPage()
+    await pm.navigateTo().datepickerPage()
+    await pm.navigateTo().smartTablePage()
+    await pm.navigateTo().toastrPage()
+    await pm.navigateTo().tooltipPage()
+})
+
+test('parameterized methods', async({page}) => {
+    const pm = new PageManager(page)
+    await pm.navigateTo().formLayoutsPage()
+    await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption("test@test.com", "Welcome1", "Option 2")
+    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox("John Smith", "John@test.com", false)
+    await pm.navigateTo().datepickerPage()
+    await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(10)
+    await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 15)
 })
